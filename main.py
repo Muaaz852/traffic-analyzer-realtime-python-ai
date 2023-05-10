0f6d94d05695d51308d661278d61c54b6df40171
Muaaz852 <gct.165103.a@gmail.com>
2023-05-10 20:28:32 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import random
+import shutil
+from sklearn.model_selection import train_test_split
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
